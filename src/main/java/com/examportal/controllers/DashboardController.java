ffd729f34497b8870a670855323896c7eeebd539
King Somu <biswajitojha152@gmail.com>
2025-10-31 23:36:17 +0530

    feat: implement quiz trail api response for user with the same quiz trail implementation for admin

--- src/main/java/com/examportal/controllers/DashboardController.java
+++ src/main/java/com/examportal/controllers/DashboardController.java
@@ -1,10 +1,15 @@
 package com.examportal.controllers;
 
 import com.examportal.dto.QuizTrailArgumentDTO;
+import com.examportal.helper.SecurityContextHelper;
+import com.examportal.models.ERole;
 import com.examportal.services.DashboardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Objects;
 
 @RestController
 @RequestMapping("/api/dashboard")
@@ -12,6 +17,9 @@ public class DashboardController {
 
     @Autowired
     private DashboardService dashboardService;
+
+    @Autowired
+    private SecurityContextHelper securityContextHelper;
 
     @GetMapping("/getDashboardSummary")
     public ResponseEntity<?> getDashboardSummary(){
@@ -22,6 +30,10 @@ public class DashboardController {
     public ResponseEntity<?> getQuizTrail(
             @ModelAttribute QuizTrailArgumentDTO quizTrailArgumentDTO
     ){
+        String role = securityContextHelper.getCurrentUserRole();
+        if(!Objects.equals(role, ERole.ADMIN.name())){
+            quizTrailArgumentDTO.setSearchByUsername(SecurityContextHolder.getContext().getAuthentication().getName());
+        }
         return ResponseEntity.ok(dashboardService.getQuizTrail(quizTrailArgumentDTO));
     }
 }
